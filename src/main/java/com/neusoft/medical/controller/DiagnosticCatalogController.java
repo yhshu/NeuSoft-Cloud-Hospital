a551c1e1e75d30cfefd47678921dcdf94b89630a
lzw429 <SHUYIHENG2015XIN>
2019-06-05 14:56:15 +0800

    update

--- src/main/java/com/neusoft/medical/controller/DiagnosticCatalogController.java
+++ src/main/java/com/neusoft/medical/controller/DiagnosticCatalogController.java
@@ -1,5 +1,6 @@
 package com.neusoft.medical.controller;
 
+import com.neusoft.medical.service.DiagnosticCatalogService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,11 +10,18 @@ import java.util.logging.Logger;
  * 诊断目录控制器
  */
 @Controller
-@RequestMapping("diagnostic_catalog")
+@RequestMapping("/diagnostic_catalog")
 public class DiagnosticCatalogController {
     private Logger log = Logger.getLogger(DiagnosticCatalogController.class);
+    private DiagnosticCatalogService diagnosticCatalogService;
 
-    // @RequestMapping("/list")
+    @RequestMapping("/list_disease_category")
+    public String findAllDiseaseCategory() {
+    }
 
+    @RequestMapping("/list_disease")
+    public String findAllDisease() {
+
+    }
 
 }
---
+++ src/main/java/com/neusoft/medical/service/DiagnosticCatalogService.java
@@ -0,0 +1,17 @@
+package com.neusoft.medical.service;
+
+import com.neusoft.medical.bean.Disease;
+import com.neusoft.medical.bean.DiseaseCategory;
+
+import java.util.List;
+
+public class DiagnosticCatalogService {
+
+    public List<DiseaseCategory> findAllDiseaseCategory() {
+
+    }
+
+    public List<Disease> findAllDisease() {
+
+    }
+}
