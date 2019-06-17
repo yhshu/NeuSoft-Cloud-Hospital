5ca0a1843e974cf4f23b5f4542e4b4de6049b4de
lzw429 <SHUYIHENG2015XIN>
2019-06-17 18:43:09 +0800

    - 处方 获取药品

--- src/main/java/com/neusoft/medical/service/impl/PrescriptionServiceImpl.java
+++ src/main/java/com/neusoft/medical/service/impl/PrescriptionServiceImpl.java
@@ -1,16 +1,31 @@
 package com.neusoft.medical.service.impl;
 
 import com.neusoft.medical.bean.Medicine;
+import com.neusoft.medical.bean.MedicineExample;
+import com.neusoft.medical.dao.MedicineMapper;
 import com.neusoft.medical.service.doctorWorkstation.PrescriptionService;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 @Service
 public class PrescriptionServiceImpl implements PrescriptionService {
+    @Resource
+    private MedicineMapper medicineMapper;
+
     @Override
     public List<Medicine> selectMedicine() {
-        // todo
-        return null;
+        List<Medicine> medicineList;
+        try {
+            MedicineExample medicineExample = new MedicineExample();
+            MedicineExample.Criteria criteria = medicineExample.createCriteria();
+            criteria.andValidEqualTo(1);
+            medicineList = medicineMapper.selectByExample(medicineExample);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+        return medicineList;
     }
 }
