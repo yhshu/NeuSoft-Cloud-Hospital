87024df33ab5b5deb6562de235895b6416832715
lzw429 <SHUYIHENG2015XIN>
2019-06-19 14:04:27 +0800

    - 查询收费项目清单

--- src/main/java/com/neusoft/medical/service/impl/MedicineDistributeServiceImpl.java
+++ src/main/java/com/neusoft/medical/service/impl/MedicineDistributeServiceImpl.java
@@ -1,15 +1,19 @@
 package com.neusoft.medical.service.impl;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.neusoft.medical.bean.ChargeForm;
 import com.neusoft.medical.bean.ChargeFormExample;
 import com.neusoft.medical.bean.Patient;
 import com.neusoft.medical.bean.Registration;
-import com.neusoft.medical.dao.ChargeFormMapper;
-import com.neusoft.medical.dao.PatientMapper;
-import com.neusoft.medical.dao.RegistrationMapper;
+import com.neusoft.medical.dao.*;
 import com.neusoft.medical.service.pharmacyWorkstation.MedicineDistributeService;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @Service
 public class MedicineDistributeServiceImpl implements MedicineDistributeService {
@@ -19,6 +23,12 @@ public class MedicineDistributeServiceImpl implements MedicineDistributeService 
     private RegistrationMapper registrationMapper;
     @Resource
     private ChargeFormMapper chargeFormMapper;
+    @Resource
+    private ChargeItemMapper chargeItemMapper;
+    @Resource
+    private DoctorMapper doctorMapper;
+
+    private Gson gson = new Gson();
 
     @Override
     public Patient selectPatient(int registrationId) {
@@ -28,10 +38,26 @@ public class MedicineDistributeServiceImpl implements MedicineDistributeService 
 
     @Override
     public String selectChargeFormList(int registrationId) {
-        ChargeFormExample chargeFormExample = new ChargeFormExample();
-//        chargeFormExample.or().andValidEqualTo(1).andRegistrationIdEqualTo();
-        // todo
-        return null;
+        String res = null;
+        try {
+            ChargeFormExample chargeFormExample = new ChargeFormExample();
+            chargeFormExample.or().andValidEqualTo(1).andRegistrationIdEqualTo(registrationId);
+            List<ChargeForm> chargeFormList = chargeFormMapper.selectByExample(chargeFormExample);
+            JsonArray chargeFormJsonArray = gson.toJsonTree(chargeFormList).getAsJsonArray();
 
+            for (JsonElement chargeFormJsonElement : chargeFormJsonArray) {
+                JsonObject chargeFormJsonObject = chargeFormJsonElement.getAsJsonObject();
+                chargeFormJsonObject.addProperty("doctorName", doctorMapper.selectByPrimaryKey(chargeFormJsonObject.get("doctorId").getAsInt()).getDoctorName());  // 医生姓名
+                chargeFormJsonObject.addProperty("chargeItemNameZh", chargeItemMapper.selectByPrimaryKey(chargeFormJsonObject.get("chargeItemId").getAsInt()).getNameZh());  // 药品中文名称
+                chargeFormJsonObject.addProperty("price", chargeItemMapper.selectByPrimaryKey(chargeFormJsonObject.get("chargeItemId").getAsInt()).getPrice());  // 药品单价
+            }
+
+            res = chargeFormJsonArray.toString();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+        return res;
     }
 }
